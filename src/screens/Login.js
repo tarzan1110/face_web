848515fabeec85559f2856da092275a75133d7fa
tarzan1110@outlook.com <tarzan1110@outlook.com>
2020-03-30 09:13:34 -0700

    file name change

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import Button from '../components/LoginButton'
+import Button from '../components/Button'
 
 class Login extends Component {
     constructor(props) {
